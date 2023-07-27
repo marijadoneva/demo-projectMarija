21007601d097bc192bab264adb8f5fb8dc024cd5
Marija <marijadoneva9@gmail.com>
2023-07-27 16:27:26 -0400

    Commit

--- src/test/java/cydeo/StepDefs.java
+++ src/test/java/cydeo/StepDefs.java
@@ -24,6 +24,9 @@ public class StepDefs {
         Driver.getDriver().manage().window().maximize();
         Driver.getDriver().get("http://etsy.com");
 
+
+
+
     }
 
     @When("^I search for \"([^\"]*)\"$")
